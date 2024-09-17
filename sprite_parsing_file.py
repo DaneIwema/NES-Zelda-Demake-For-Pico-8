c1071e3d4ebf70dfd9e65da8b0e1df39b9f45700
DaneIwema <daneiwema@gmail.com>
2024-09-17 17:38:41 -0500

    Project so far

---
+++ sprite_parsing_file.py
@@ -0,0 +1,57 @@
+from PIL import Image
+
+colors=[(0,0,0), (1,1,1), (1,1,1), (1,1,1), (200,76,12), (1,1,1), (116,116,116), (252,252,252), (1,1,1), (1,1,1), (1,1,1), (0,168,0), (32,56,236), (1,1,1), (1,1,1), (252,216,168)]
+
+img_sprites=[]
+
+#TODO
+#def rle_encode(data):
+
+#TODO
+#def huffman_encode(data):
+
+def location(list, img):
+    if img in list:
+        return list.index(img)
+    list.append(img)
+    return len(list) - 1
+
+def get_room(location=0):
+    im = Image.open("overworld.png")
+    x1 = location % 16 * 256
+    y1 = location // 16 * 168
+    return  im.crop((x1, y1, x1 + 256, y1 + 168))
+
+def parse_map(im, num=0):
+    return [location(img_sprites, im.crop((x % 32 * 8, (x // 32 * 8), (x % 32 * 8) + 8, (x // 32 * 8) + 8))) for x in range(672)]
+
+def parse_sprites():
+    return [[colors.index(sprite.load()[i,j]) for j in range(8) for i in range(8)] for sprite in img_sprites]
+
+def condense_sprites(sprites_data):
+    return [[sprite[x + 1]<<4 | sprite[x] for x in range(0,64,2)] for sprite in sprites_data]
+
+def write_to_file(f,map,sprites_data,num):
+    strg="levels = {"
+    f.write(strg)
+    for x in map:
+        f.write(str(x+1))
+        f.write(", ")
+    f.write("}\nsprites = {")
+    for sprite in sprites_data:
+        f.write("{")
+        for x in sprite:
+            f.write(str(x))
+            f.write(", ")
+        f.write("}, ")
+    f.write("}\n")
+
+def parse_room(num):
+    room = get_room(num)
+    map = parse_map(room, num)
+    sprites_data = parse_sprites()
+    condensensed_sprites = condense_sprites(sprites_data)
+    f = open("room.p8", "w")
+    write_to_file(f,map,condensensed_sprites,num)
+
+parse_room(51)
---
+++ zelda_demake.p8
@@ -0,0 +1,67 @@
+pico-8 cartridge // http://www.pico-8.com
+version 42
+__lua__
+#include room.p8
+
+function _init()
+    room_width=32
+    room_height=22
+    cam_x,cam_y=0,0
+    map_x,map_y=0,0
+    load_sprites()
+    load_map()
+end
+
+function _update()
+    if (btn(0)) cam_x-=1
+    if (btn(1)) cam_x+=1
+    if (btn(2)) cam_y-=1
+    if (btn(3)) cam_y+=1
+
+    if (btn(4))room=1
+    if (btn(5))room=2
+
+    check_map_boundry()
+end
+
+function _draw()
+    cls()
+    
+    camera(cam_x, cam_y)
+
+    -- poke(512, 254)
+    -- sspr(0,0,128,32)
+
+    map(0,0,0,0,18,18)
+end
+-->8
+function load_sprites()
+    for ind,sprite in pairs(sprites) do
+        for i=0, 8, 1 do
+            poke(ind * 4 + (448 * flr(ind / 16) + (64 * i)), unpack(sprite, 1+(i*4), 1+(i*4)+4))
+        end
+    end 
+end
+function load_map()
+    for i=0,17,1 do
+        poke(8192+(128*i), unpack(room, i*32+1+(map_y*32)+map_x, i*32+18+(map_y*32)+map_x))
+    end
+end
+function check_map_boundry()
+
+    if cam_x==17 and map_x!=14 then map_x+=1 cam_x=8 
+    elseif cam_x==17 and map_x==14 then cam_x=16 end
+
+    if cam_y==16 and map_y!=3 then map_y+=1 cam_y=8 
+    elseif cam_y==17 and map_y==3 then cam_y=16 end
+
+    if cam_x==-1 and map_x!=0 then map_x-=1 cam_x=8 
+    elseif cam_x==-1 and map_x==0 then cam_x=0 end
+
+    if cam_y==-1 and map_y!=0 then map_y-=1 cam_y=8 
+    elseif cam_y==-1 and map_y==0 then cam_y=0 end
+
+    for i=0,17,1 do
+        poke(8192+(128*i), unpack(room, i*32+1+(map_y*32)+map_x, i*32+18+(map_y*32)+map_x))
+    end
+end
